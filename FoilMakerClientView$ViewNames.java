8a94181f6a80f1dc25e1840d2d3a73131f1744af
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-11-16 13:52:46 -0500

    Update FoilMakerClientView$ViewNames.java

--- FoilMakerClientView$ViewNames.java
+++ FoilMakerClientView$ViewNames.java
@@ -4,9 +4,3 @@
   
   private FoilMakerClientView$ViewNames() {}
 }
-
-
-/* Location:              /Users/brandonnguyen/Desktop/PurdueStuffs/CS18000/Foiler/Foilmaker.client.jar!/FoilMakerClientView$ViewNames.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       0.7.1
- */
