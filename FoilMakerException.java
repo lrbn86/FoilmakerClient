99e26a69b2b02103e155474103668a1bf52cb499
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-11-16 13:53:33 -0500

    Update FoilMakerException.java

--- FoilMakerException.java
+++ FoilMakerException.java
@@ -1,18 +1,16 @@
-/*    */ public class FoilMakerException
-/*    */   extends Exception
-/*    */ {
-/*    */   private FoilMakerNetworkProtocol.MSG_DETAIL_T msgDetail;
-/*    */   
-/*    */   public FoilMakerException(FoilMakerNetworkProtocol.MSG_DETAIL_T msgDetail)
-/*    */   {
-/*  8 */     super("" + msgDetail);
-/*  9 */     this.msgDetail = msgDetail; }
-/*    */   
-/* 11 */   public FoilMakerNetworkProtocol.MSG_DETAIL_T getType() { return this.msgDetail; }
-/*    */ }
-
-
-/* Location:              /Users/brandonnguyen/Desktop/PurdueStuffs/CS18000/Foiler/Foilmaker.client.jar!/FoilMakerException.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       0.7.1
- */
+public class FoilMakerException
+  extends Exception
+{
+  private FoilMakerNetworkProtocol.MSG_DETAIL_T msgDetail;
+  
+  public FoilMakerException(FoilMakerNetworkProtocol.MSG_DETAIL_T msgDetail)
+  {
+    super("" + msgDetail);
+    this.msgDetail = msgDetail;
+  }
+  
+  public FoilMakerNetworkProtocol.MSG_DETAIL_T getType()
+  {
+    return this.msgDetail;
+  }
+}
