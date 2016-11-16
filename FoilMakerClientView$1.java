f3f0ab1160551fae4c62dcc55d90794440a4669b
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-11-16 13:52:13 -0500

    Update FoilMakerClientView$1.java

--- FoilMakerClientView$1.java
+++ FoilMakerClientView$1.java
@@ -1,120 +1,27 @@
-/*     */ import java.awt.event.WindowAdapter;
-/*     */ import java.awt.event.WindowEvent;
-/*     */ import java.io.IOException;
-/*     */ import javax.swing.JOptionPane;
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ class FoilMakerClientView$1
-/*     */   extends WindowAdapter
-/*     */ {
-/*     */   FoilMakerClientView$1(FoilMakerClientView this$0, FoilMakerClient paramFoilMakerClient) {}
-/*     */   
-/*     */   public void windowClosing(WindowEvent e)
-/*     */   {
-/* 102 */     int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?", null, 0, 3);
-/*     */     
-/*     */ 
-/* 105 */     if (response == 0) {
-/*     */       try {
-/* 107 */         this.val$controller.logout();
-/*     */       } catch (IOException ex) {
-/* 109 */         ex.printStackTrace();
-/*     */       }
-/* 111 */       System.exit(0);
-/*     */     }
-/*     */   }
-/*     */ }
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 
-
-/* Location:              /Users/brandonnguyen/Desktop/PurdueStuffs/CS18000/Foiler/Foilmaker.client.jar!/FoilMakerClientView$1.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       0.7.1
- */
+class FoilMakerClientView$1
+  extends WindowAdapter
+{
+  FoilMakerClientView$1(FoilMakerClientView this$0, FoilMakerClient paramFoilMakerClient) {}
+  
+  public void windowClosing(WindowEvent e)
+  {
+    int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?", null, 0, 3);
+    if (response == 0)
+    {
+      try
+      {
+        this.val$controller.logout();
+      }
+      catch (IOException ex)
+      {
+        ex.printStackTrace();
+      }
+      System.exit(0);
+    }
+  }
+}
