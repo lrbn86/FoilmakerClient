f5ceb6b1be5cfc98e095e8579b4055ad537d4227
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-11-16 13:53:51 -0500

    Update FoilMakerNetworkProtocol$MSG_DETAIL_T.java

--- FoilMakerNetworkProtocol$MSG_DETAIL_T.java
+++ FoilMakerNetworkProtocol$MSG_DETAIL_T.java
@@ -4,9 +4,3 @@ public enum FoilMakerNetworkProtocol$MSG_DETAIL_T
   
   private FoilMakerNetworkProtocol$MSG_DETAIL_T() {}
 }
-
-
-/* Location:              /Users/brandonnguyen/Desktop/PurdueStuffs/CS18000/Foiler/Foilmaker.client.jar!/FoilMakerNetworkProtocol$MSG_DETAIL_T.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       0.7.1
- */
